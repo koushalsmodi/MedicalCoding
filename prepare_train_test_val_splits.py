746a8a5171c568ac96439fe1d62c8a7cbfbec134
Koushal S Modi <124229657+koushalsmodi@users.noreply.github.com>
2024-04-22 17:30:16 -0700

    Update prepare_train_test_val_splits.py

--- prepare_train_test_val_splits.py
+++ prepare_train_test_val_splits.py
@@ -11,7 +11,7 @@ diagnoses10 = diagnoses.query('icd_version == 10')
 
 fname = data_dir + 'discharge.csv'
 discharge_full = pd.read_csv(data_dir + 'discharge.csv')
- 16 
+
  17 # Here we should throw out the irrelevant doctor's notes.
  18 relevant_hadm_ids = list(diagnoses10['hadm_id'].unique())
  19 discharge = discharge_full[discharge_full['hadm_id'].isin(relevant_hadm_ids)]
