91c8e3fa39fc9c47cc9bdba19f58d6d0baaabbb4
Koushal S Modi <124229657+koushalsmodi@users.noreply.github.com>
2024-04-22 17:31:30 -0700

    Update prepare_train_test_val_splits.py

--- prepare_train_test_val_splits.py
+++ prepare_train_test_val_splits.py
@@ -20,7 +20,7 @@ discharge['text_length'] = discharge['text'].apply(len)
 discharge_sorted = discharge.sort_values(by='text_length', ascending=False)
 unique_subject_ids = discharge['subject_id'].unique()
 
- 26 subject_ids_train, subject_ids_val = sk.model_selection.train_test_split(unique_subject_ids, train_size=.8)
+subject_ids_train, subject_ids_val = sk.model_selection.train_test_split(unique_subject_ids, train_size=.8)
  27 subject_ids_val, subject_ids_test = sk.model_selection.train_test_split(subject_ids_val, train_size=.5)
  28 
  29 pdb.set_trace()
